0c4f9c114c5d0a938694c022fe3a0f12fc714c5f
192114 <shadowSun192114@gmail.com>
2022-08-24 18:21:54 +0800

    css module ts 配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import { lazy, Suspense } from 'react'
 
+import styles from './App.module.css'
+
 const Login = lazy(() => import('./pages/Login'))
 
 function App() {
   return (
     <Suspense fallback={null}>
+      <div className={styles.title}>title</div>
       <Routes>
         <Route path="login" element={<Login />} />
       </Routes>
