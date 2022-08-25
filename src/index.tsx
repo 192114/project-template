4af2d80247975eac1c9e473b79799debf84dc7b0
192114 <shadowSun192114@gmail.com>
2022-08-25 17:49:32 +0800

    切换vars.css 导入位置

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 
 import App from './App'
-import './assets/styles/vars.css'
 import './index.css'
 
 const rootElement = document.createElement('div')
