d2c3c680f14a3a10debbb10c495cea07df38c1a5
192114 <shadowSun192114@gmail.com>
2022-08-25 17:17:00 +0800

    postcss css变量配置

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # project-template
 
 基于 webpack 的项目模板
+
+- [ ] postcss-preset-env ("importFrom" and "exportTo" will be removed in a future version of postcss-custom-properties.) **[讨论](https://github.com/csstools/postcss-plugins/discussions/192)**
--- postcss.config.js
+++ postcss.config.js
@@ -6,12 +6,12 @@ module.exports = {
         fix: true,
       }
     ],
-    [
-      'cssnano',
-      {
-        preset: 'default',
-      }
-    ],
+    // [
+    //   'cssnano',
+    //   {
+    //     preset: 'default',
+    //   }
+    // ],
     [
       'postcss-normalize'
     ],
@@ -21,7 +21,14 @@ module.exports = {
         // 文档 https://preset-env.cssdb.org/features/#stage-3
         features: {
           'nesting-rules': true, // 嵌套语法 stage 0 语法
+          // discussions: https://github.com/csstools/postcss-plugins/discussions/192
+          // "importFrom" and "exportTo" will be removed in a future version of postcss-custom-properties.
+          // 关闭废弃警告
+          'custom-properties': {
+            "disableDeprecationNotice": true
+          },
         },
+        importFrom: './src/assets/styles/vars.css',
       }
     ],
   ],
